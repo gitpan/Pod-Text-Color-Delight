db7b29251023ac6e67c024d89c96839457a23539
Taiki Kawakami <moznion@gmail.com>
2013-10-26 08:58:09 +0000

    Import of MOZNION/Pod-Text-Color-Delight-0.01 from CPAN.
    gitpan-cpan-distribution: Pod-Text-Color-Delight
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         MOZNION/Pod-Text-Color-Delight-0.01.tar.gz
    gitpan-cpan-author:       MOZNION
    gitpan-cpan-maturity:     released

---
+++ README.md
@@ -0,0 +1,50 @@
+# NAME
+
+Pod::Text::Color::Delight - Delight Light Highlight the POD
+
+# SYNOPSIS
+
+    use Pod::Text::Color::Delight;
+
+    my $parser = Pod::Text::Color::Delight->new;
+    $parser->parse_from_filehandle("/path/to/your.pod");
+
+# DESCRIPTION
+
+Pod::Text::Color::Delight is a subclass of [Pod::Text::Color](http://search.cpan.org/perldoc?Pod::Text::Color) that highlights the pod text by using ANSI color escape.
+
+Highlight of this module is a little radical in comparison with parent. For example, this module highlights headlines, links, and other several elements.
+Additionally, this module also highlights Perl code (e.g. SYNOPSIS).
+
+Basic usage is the same as [Pod::Text::Color](http://search.cpan.org/perldoc?Pod::Text::Color). So please refer it.
+
+# CONFIGURATION AND ENVIRONMENT
+
+You can configure colors as you like!
+What is necessary is just to put a `.pod\_text\_color\_delight` (this is configuration file) on your home directory.
+
+The example of a configuration file should look at <a href="https://github.com/moznion/Pod-Text-Color-Delight/blob/master/sample/configurations">samples</a>.
+
+
+
+If you want to force the default settings, please set true value into __POD\_TEXT\_COLOR\_DELIGHT\_DEFAULT__ of environment variable.
+
+# REQUIREMENTS
+
+This module requires the terminal which is able to use 256 colors. If you use the terminal that does not meet the conditions,
+this module will not highlight texts completely.
+
+# SEE ALSO
+
+[Pod::Text](http://search.cpan.org/perldoc?Pod::Text), [Pod::Text::Color](http://search.cpan.org/perldoc?Pod::Text::Color)
+
+# LICENSE
+
+Copyright (C) moznion.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+moznion <moznion@gmail.com>
---
+++ lib/Pod/Text/Color/Delight.pm
@@ -0,0 +1,192 @@
+package Pod::Text::Color::Delight;
+use 5.008005;
+use strict;
+use warnings;
+use Term::ANSIColor qw(colored);
+use File::Spec::Functions qw(catfile);
+use Syntax::Highlight::Perl::Improved;
+use parent 'Pod::Text::Color';
+
+our $VERSION = '0.01';
+
+use constant COLOR_TABLE => {
+    head1  => 'bright_cyan',
+    head2  => 'reset bold',
+    head3  => '',
+    head4  => '',
+    bold   => 'reset bold',
+    file   => 'bright_green',
+    italic => 'reset italic',
+    link   => 'rgb045',
+    code   => {
+        Character         => 'cyan',
+        String            => 'rgb542',
+        Quote             => 'rgb542',
+        Label             => 'rgb542',
+
+        Builtin_Function  => 'bright_red',
+        Builtin_Operator  => 'bright_red',
+
+        Keyword           => 'bright_red',
+        Package           => 'rgb345',
+
+        Subroutine        => 'rgb454',
+        Bareword          => 'white',
+        Symbol            => 'white',
+        Operator          => 'white',
+        Number            => 'white',
+
+        Variable_Hash     => 'rgb520',
+        Variable_Array    => 'rgb520',
+        Variable_Scalar   => 'rgb520',
+        Variable_Typeglob => 'rgb520',
+
+        Comment_Normal    => 'grey10',
+        Comment_POD       => 'grey10',
+        DATA              => 'grey10',
+        Directive         => 'bright_green',
+    },
+};
+
+sub new {
+    my $class = shift;
+
+    my $self = $class->SUPER::new;
+
+    my $color_table_file = catfile($ENV{HOME}, '.pod_text_color_delight');
+    my $color_table = COLOR_TABLE;
+    if (!$ENV{POD_TEXT_COLOR_DELIGHT_DEFAULT} && -f $color_table_file) {
+        $color_table = do $color_table_file;
+    }
+
+    $self->{color_table} = $color_table;
+    return $self;
+}
+
+sub cmd_head1 {
+    my ($self, $attrs, $text) = @_;
+    $self->SUPER::cmd_head1($attrs, colored($text, $self->_select_color('head1')));
+}
+
+sub cmd_head2 {
+    my ($self, $attrs, $text) = @_;
+    $self->SUPER::cmd_head2($attrs, colored($text, $self->_select_color('head2')));
+}
+
+sub cmd_head3 {
+    my ($self, $attrs, $text) = @_;
+    $self->SUPER::cmd_head3($attrs, colored($text, $self->_select_color('head3')));
+}
+
+sub cmd_head4 {
+    my ($self, $attrs, $text) = @_;
+    $self->SUPER::cmd_head4($attrs, colored($text, $self->_select_color('head4')));
+}
+
+sub cmd_b {
+    my ($self, $attrs, $text) = @_;
+    $self->SUPER::cmd_b($attrs, colored($text, $self->_select_color('bold')));
+}
+
+sub cmd_f {
+    my ($self, $attrs, $text) = @_;
+    $self->SUPER::cmd_f($attrs, colored($text, $self->_select_color('file')));
+}
+
+sub cmd_c {
+    my ($self, $attrs, $text) = @_;
+    $self->SUPER::cmd_c($attrs, $self->_highlight_code($attrs, $text));
+}
+
+sub cmd_i {
+    my ($self, $attrs, $text) = @_;
+    $self->SUPER::cmd_i($attrs, colored($text, $self->_select_color('italic')));
+}
+
+sub cmd_l {
+    my ($self, $attrs, $text) = @_;
+    $self->SUPER::cmd_l($attrs, colored($text, $self->_select_color('link')));
+}
+
+sub cmd_verbatim {
+    my ($self, $attrs, $text) = @_;
+    $self->SUPER::cmd_verbatim($attrs, $self->_highlight_code($attrs, $text));
+}
+
+sub _highlight_code {
+    my ($self, $attrs, $text) = @_;
+
+    my $formatter = Syntax::Highlight::Perl::Improved->new;
+
+    while (my ($type, $style) = each %{$self->_select_color('code')}) {
+        $formatter->set_format($type, [Term::ANSIColor::color($style), Term::ANSIColor::color('reset')]);
+    }
+
+    return $formatter->format_string($text);
+}
+
+sub _select_color {
+    my ($self, $element) = @_;
+
+    return $self->{color_table}->{$element} || COLOR_TABLE->{$element} || 'reset';
+}
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Pod::Text::Color::Delight - Delight Light Highlight the POD
+
+=head1 SYNOPSIS
+
+    use Pod::Text::Color::Delight;
+
+    my $parser = Pod::Text::Color::Delight->new;
+    $parser->parse_from_filehandle("/path/to/your.pod");
+
+=head1 DESCRIPTION
+
+Pod::Text::Color::Delight is a subclass of L<Pod::Text::Color> that highlights the pod text by using ANSI color escape.
+
+Highlight of this module is a little radical in comparison with parent. For example, this module highlights headlines, links, and other several elements.
+Additionally, this module also highlights Perl code (e.g. SYNOPSIS).
+
+Basic usage is the same as L<Pod::Text::Color>. So please refer it.
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+=begin html
+
+You can configure colors as you like!
+What is necessary is just to put a F<.pod_text_color_delight> (this is configuration file) on your home directory.
+
+The example of a configuration file should look at <a href="https://github.com/moznion/Pod-Text-Color-Delight/blob/master/sample/configurations">samples</a>.
+
+=end html
+
+
+If you want to force the default settings, please set true value into B<POD_TEXT_COLOR_DELIGHT_DEFAULT> of environment variable.
+
+=head1 REQUIREMENTS
+
+This module requires the terminal which is able to use 256 colors. If you use the terminal that does not meet the conditions,
+this module will not highlight texts completely.
+
+=head1 SEE ALSO
+
+L<Pod::Text>, L<Pod::Text::Color>
+
+=head1 LICENSE
+
+Copyright (C) moznion.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+moznion E<lt>moznion@gmail.comE<gt>
+
+=cut
